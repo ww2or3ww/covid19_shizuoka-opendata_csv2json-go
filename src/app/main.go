714bef31598544c1c73a81e7de750d77b67696fd
ww2or3ww <w2or3w@gmail.com>
2021-06-13 23:56:02 +0000

    use env value for log lv

--- src/app/main.go
+++ src/app/main.go
@@ -7,25 +7,34 @@ import (
 
 	"app/csv2json"
 	"app/utils/logger"
+	"os"
+	"strconv"
 	"time"
 )
 
+// クエリパラメータが無かった場合のデフォルト(浜松市)
 const defaultTypes = "main_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,main_summary:92f9ebcd-a3f1-4d5d-899b-d69214294a45,patients:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,patients_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,inspection_persons:d4827176-d887-412a-9344-f84f161786a2,contacts:1b57f2c0-081e-4664-ba28-9cce56d0b314"
 
+// APIアドレス(ふじの国オープンデータカタログ)
 const opendataApiUrl = "https://opendata.pref.shizuoka.jp/api/package_show"
 
+// AWS Lambda エンドポイント
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	timeStart := time.Now()
 	logger.Debugs(request)
 	logger.Infof("query = %s\n", request.QueryStringParameters)
 	logger.Infof("body  = \n%s\n", request.Body)
 
+	// クエリパラメータ取得
 	queryStrPrm := request.QueryStringParameters["type"]
 	if queryStrPrm == "" {
 		queryStrPrm = defaultTypes
 	}
+
+	// csv2json
 	mapData := csv2json.Process(opendataApiUrl, queryStrPrm)
 
+	// mapをインデント付きのJSONに整形してBodyとして返す
 	jsonIndent, err := json.MarshalIndent(mapData, "", "   ")
 	if err != nil {
 		return createErrorResponse("error occured.", err)
@@ -40,6 +49,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}, nil
 }
 
+// エラー時のレスポンス
 func createErrorResponse(message string, err error) (events.APIGatewayProxyResponse, error) {
 	logger.Errors(message, err)
 	return events.APIGatewayProxyResponse{
@@ -48,10 +58,20 @@ func createErrorResponse(message string, err error) (events.APIGatewayProxyRespo
 	}, err
 }
 
+// mainメソッドの前に呼ばれる初期化処理
 func init() {
-	logger.LogInitialize(logger.Info, 25)
+	// LogLvを環境変数から取得してLog初期設定する
+	logLv := logger.Error
+	envLogLv := os.Getenv("LOG_LEVEL")
+	logger.Infos(envLogLv)
+	if envLogLv != "" {
+		n, _ := strconv.Atoi(envLogLv)
+		logLv = logger.LogLv(n)
+	}
+	logger.LogInitialize(logLv, 25)
 }
 
+// アプリケーションエンドポイント
 func main() {
 	logger.Infos("=== START ===")
 	lambda.Start(handler)
