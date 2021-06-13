3318262900a9f54751ea669053ef6424621e906b
ww2or3ww <w2or3w@gmail.com>
2021-06-13 14:43:12 +0000

    default type for hamamatsu

--- src/app/main.go
+++ src/app/main.go
@@ -9,6 +9,8 @@ import (
 	"app/utils/logger"
 )
 
+const defaultTypes = "main_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,main_summary:92f9ebcd-a3f1-4d5d-899b-d69214294a45,patients:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,patients_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,inspection_persons:d4827176-d887-412a-9344-f84f161786a2,contacts:1b57f2c0-081e-4664-ba28-9cce56d0b314"
+
 const opendataApiUrl = "https://opendata.pref.shizuoka.jp/api/package_show"
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -17,6 +19,9 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	logger.Infof("body  = \n%s\n", request.Body)
 
 	queryStrPrm := request.QueryStringParameters["type"]
+	if queryStrPrm == "" {
+		queryStrPrm = defaultTypes
+	}
 	mapData := csv2json.Process(opendataApiUrl, queryStrPrm)
 
 	jsonIndent, err := json.MarshalIndent(mapData, "", "   ")
